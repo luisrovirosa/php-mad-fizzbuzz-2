dc2eeb7b95d282562f460754cda217a4279664d0
Luis Rovirosa <luisrovirosa@gmail.com>
2014-07-16 20:32:36 +0200

    Test first position

--- src/PHPMad/Foo.php
+++ src/PHPMad/Foo.php
@@ -7,7 +7,7 @@ class Foo
 
     public function run()
     {
-        return array_fill(0, 100, '');
+        return array_fill(0, 100, 1);
     }
 
 }
--- tests/PHPMad/Tests/FooTest.php
+++ tests/PHPMad/Tests/FooTest.php
@@ -8,16 +8,22 @@ class FooTest extends TestCase
 {
 
     protected $object;
+    private $fizzBuzz;
 
     protected function setUp()
     {
         $this->object = new Foo();
+        $this->fizzBuzz = $this->object->run();
     }
 
     function testLength()
     {
+        $this->assertCount(100, $this->fizzbuzz);
+    }
 
-        $this->assertCount(100, $this->object->run());
+    function testNormalNumbersAreThemselves()
+    {
+        $this->assertEquals(1, $this->fizzBuzz[0]);
     }
 
 }
